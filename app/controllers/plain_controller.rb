0dc95b502b2ef96e28ad1d6ee0736ed4fc91b115
AlexV348 <AlexV348@gmail.com>
2015-04-20 12:49:45 -0700

    Index page working

--- app/controllers/plain_controller.rb
+++
@@ -1,5 +0,0 @@
-class PlainController < ApplicationController
-  def index
-    render "index"
-  end
-end
---
+++ app/controllers/plains_controller.rb
@@ -0,0 +1,5 @@
+class PlainsController < ApplicationController
+  def index
+    render "index"
+  end
+end
